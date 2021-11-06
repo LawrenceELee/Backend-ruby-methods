23a1180341f716be5576a67a368853bc00a5cedc
Lawrence L <Lawrence.E.Lee@gmail.com>
2021-11-05 21:56:44 -0700

    Finished question 1 divisible.rub

--- divisible.rb
+++ divisible.rb
@@ -1,1 +1,24 @@
+=begin
+=end
 
+def isDivisibleByTwoThreeFive(num)
+  true if (num % 2 == 0) or (num % 3 == 0) or (num % 5 == 0) 
+end
+
+def divisible
+  result = []
+
+  for n in (1..100) do
+    result.push(n) if isDivisibleByTwoThreeFive(n)
+  end
+
+  return result
+end
+
+#p divisible()
+
+#p isDivisibleByTwoThreeFive(1)
+#p isDivisibleByTwoThreeFive(10)
+#p isDivisibleByTwoThreeFive(97)
+
+print divisible()
